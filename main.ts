ad03aea5fa0d53ebd34f768e05dbeb3d2ec4adab
frippayelen24-lab <frippayelen24@gmail.com>
2025-11-04 12:27:01 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+let Mano = 0
+input.onGesture(Gesture.Shake, function () {
+    Mano = randint(1, 3)
+    if (Mano == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (Mano == 2) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
